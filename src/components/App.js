f01eefae0c8738df31d95cc91a13dbfcd7fe6fab
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-08-28 13:39:07 -0300

    Refctored Options component and css import

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import TopBar from "./TopBar";
 import Content from "./Content";
 import BottomBar from "./BottomBar";
 
+import "../css/reset.css";
+import "../css/style.css";
+
 export default function App() {
     return (
         <div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,4 +1,3 @@
-import { func } from 'prop-types';
 import React from 'react';
 import fetchOptions from '../data';
 
@@ -12,8 +11,8 @@ export default function Options(props){
                 <Option 
                     key = {index} 
                     img = {option.img} 
-                    name={option.name} d
-                    escription = {option.description} 
+                    name={option.name} 
+                    description = {option.description} 
                     price ={option.price}
                 />
             ))}
@@ -23,30 +22,51 @@ export default function Options(props){
 
 function Option(props){
 
-    const [selectedOption, setSelectedOption] = React.useState("");
+    const [selectedOption, setSelectedOption] = React.useState("");   
+    const [amount, setAmount] = React.useState(0);  
 
-    function ItemCounter(){
-        const [valor, setValor] = React.useState(1);  
-        if (valor === 0){
-            setSelectedOption("")
-        }  
-        return(
-            <p className="item-counter">
-                <span onClick={() => setValor(valor <= 0 ? 0 : valor - 1)} style={{color:'red'}}>-</span>
-                {` ${valor} `}
-                <span onClick={() => setValor(valor + 1)} style={{color:'green'}}>+</span>
-            </p>
-        )
+    function selectOption(){
+        if(amount === 0){
+            setAmount(1);
+            setSelectedOption("selectedOption")
+        }
     }
 
     return (
-        <button className={`option ${selectedOption}`} onClick={() => setSelectedOption("selectedOption")}>
+        <button className={`option ${selectedOption}`} onClick={selectOption}>
             <img src={props.img}></img>
             <h1 className="product-name">{props.name}</h1>
             <p className="product-description">{props.description}</p>
             <p className="product-price">{props.price}</p>
-            <ItemCounter/>
+            <ItemCounter
+                amount = {amount}
+                setAmount = {setAmount}
+                selectedOption = {selectedOption}
+                setSelectedOption = {setSelectedOption}
+            />
         </button>
     );
 }
 
+function ItemCounter(props){ 
+
+    function decrease(amount){
+
+        if (amount === 1) {
+            props.setSelectedOption("");
+        }
+        props.setAmount(amount-1)
+    }
+
+    function increase(amount){
+        props.setAmount(amount + 1)
+    }
+
+    return(
+        <p className="item-counter">
+            <span onClick={() => decrease(props.amount)} style={{color:'red'}}>-</span>
+            {` ${props.amount} `}
+            <span onClick={() => increase(props.amount)} style={{color:'green'}}>+</span>
+        </p>
+    )
+}
